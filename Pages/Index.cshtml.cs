4cfcce46745f1afc525709304bced056b92beb2d
Dany-cm <danycmwow@gmail.com>
2023-10-21 11:02:36 +0200

    Moved Index code to Recipe

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,34 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using RecipeMate.Models;
-using RecipeMate.Pages.Forms;
-using RecipeMate.Repositories;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RecipeMate.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly RecipeRepository _recipes;
-
-        public IndexModel(RecipeRepository recipes)
-        {
-            _recipes = recipes;
-        }
-
-        [BindProperty] public IndexSearchForm SearchForm { get; set; }
-
-        public List<RecipeModel>? Recipes { get; set; }
-        
-        public async Task<IActionResult> OnPost()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-            
-            Recipes = await _recipes.GetRecipesByIngredients(SearchForm.Ingredients!);
-
-            return Page();
-        }
     }
 }
